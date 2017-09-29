bfa8302b1d01ab4ae5dd54db4e6b211f1aaba9e3
Lucy Tan <lutan07@gmail.com>
2017-09-29 10:36:25 -0700

    added background

---
+++ Roovel/StartingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartingViewController.swift
+//  Roovel
+//
+//  Created by Lucy Tan on 9/28/17.
+//  Copyright © 2017 Lucy Tan. All rights reserved.
+//
+
+import UIKit
+
+class StartingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
